94115d158482d4c87e69ad43b72b2b9120237694
Arad Aryan <javadjavad541@gmail.com>
2020-01-09 19:08:20 +0330

    Replaced String.Replace with Regex

--- CloneSP/Program.cs
+++ CloneSP/Program.cs
@@ -16,18 +16,19 @@ namespace CloneSP
         /// Directory of Place That Program Start Reading SQL files Form There
         /// </summary>
         private static string mainPath =
-            @"C:\Users\faranam\Desktop\Exam\05 - clone SP\files";
+            @"C:\Users\faranam\Desktop\Desktop\Exam\05 - clone SP\files";
 
         /// <summary>
-        /// Directory of Place Where Program Store Changes There 
+        /// Directory of Place Where Program Store Changes There
         /// </summary>
         private static string outputPath =
             @"C:\Users\faranam\Desktop\Exam\05 - clone SP\files\output\";
+
         #endregion Fields
 
-        #region Renaming Context Funtion
+        #region Renaming Context Function
         /// <summary>
-        /// Main Funtion => Start Renaming Context
+        /// Main Function => Start Renaming Context
         /// </summary>
         public static void RenameFiles()
         {
@@ -45,62 +46,27 @@ namespace CloneSP
                 var context = sr.ReadToEnd();
 
                 ///Replace
-                context = context.Replace("BaseInfo", "AltBaseInfo");
-                context = context.Replace("CustInfo", "AltCustInfo");
+                context = Regex.Replace(context, "BaseInfo", "AltBaseInfo", RegexOptions.IgnoreCase);
+                context = Regex.Replace(context, "CustInfo", "AltCustInfo", RegexOptions.IgnoreCase);
+                context = Regex.Replace(context, "(create procedure)", "ALTER PROC", RegexOptions.IgnoreCase);
+                context = Regex.Replace(context, "(create proce)", "ALTER PROCEDURE ", RegexOptions.IgnoreCase);
+                context = Regex.Replace(context, @"(\sdbo.\[\w+)(\b)", "$1_Alt", RegexOptions.IgnoreCase);
+                context = Regex.Replace(context, @"(\sdbo.\w+)(\b)", "$1_Alt", RegexOptions.IgnoreCase);
 
-                ///Extract DB Name
-                string dboName = "";
-                var dbName = new string[context.Length];
-                dbName = context.Split(' ');
-                foreach (var value in dbName)
-                {
-                    if (value.Contains("dbo."))
-                    {
-                        dboName = value;
-                        break;
-                    }
-                    else dboName = "404";
-                }
-
-                ///Error Hnadling
-                if (dboName == "404")
-                    break;
-
-                ///Replace DB Name
-                else
-                {
-                    if (dboName.Contains(']'))
-                    {
-                        int index = dboName.IndexOf(']');
-                        dboName.Replace("]", "_alt]");
-                    }
-
-                    if (dboName.Contains("\r"))
-                    {
-                        string[] str = new string[2];
-                        string charIndex = "\r";
-                        str = dboName.Split(charIndex.ToCharArray());
-                        context = context.Replace(str[0], str[0].Replace("]", "_alt]"));
-                    }
-
-                    if (context.Contains(dboName))
-                    {
-                        string[] str = new string[2];
-                        string charIndex = "\r";
-                        str = dboName.Split(charIndex.ToCharArray());
-                        context = context.Replace(str[0], str[0] + "_alt");
-                    }
-
-                    ///Write To File
-                    sw = new StreamWriter(outputPath + Path.GetFileName(item));
-                    sw.Write(context);
-                    sw.Flush();
-                }
+                ///Write To File
+                sw = new StreamWriter(outputPath + Path.GetFileName(item));
+                sw.Write(context);
+                sw.Flush();
             }
         }
-        #endregion Renaming Context Funtion
+
+        #endregion Renaming Context Function
 
         #region Main Function
+        /// <summary>
+        /// Main Function
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             RenameFiles();
